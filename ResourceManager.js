873d6500554fb3184840e61ff2167e094a0ffd4a
niklassc <niklassc@student.ethz.ch>
2025-04-10 19:43:54 +0200

    add ResourceManager

---
+++ ResourceManager.js
@@ -0,0 +1,25 @@
+export default class ResourceManager {
+	/**
+	 * Key-value store of sprites
+	 */
+	static #sprites = {};
+
+
+	static getSpriteFromPath(path) {
+		// sprite exists
+		if (this.#sprites[path] !== undefined){
+			return this.#sprites[path];
+		}
+
+		// sprite don't exists → load
+		let sprite = new Image();
+		sprite.src = path;
+
+		// TODO ensure sprite is loaded
+		// await sprite.decode();
+
+		this.#sprites[path] = sprite;
+
+		return sprite
+	}
+}
