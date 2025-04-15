fd7b224ac920c14c5296e9daf8c18762c6608a54
niklassc <niklas@schmatloch.de>
2025-04-15 17:48:10 +0200

    initial clickable observer, add button to engine, destroy objects when room is left

--- Input.js
+++ Input.js
@@ -6,10 +6,11 @@ import * as collision from "./functions/collision.js";
 // TODO make static?
 
 export default class Input {
+	#x; #y;
 	constructor(g) {
 		this.g = g;
-		this.x = 0;
-		this.y = 0;
+		this.#x = 0;
+		this.#y = 0;
 
 		this.selected = undefined;
 		this.selectedTouch = undefined; // TODO merge again?
@@ -32,22 +33,53 @@ export default class Input {
 		  * List of clickable objects
 		  * @type {Array.<PhysicalEntity>}
 		*/
-		let clickable = [];
+		this.clickable = [];
 	}
 
 	/**
 	 * Register obj as clickable
 	 *
 	 * @param {PhysicalEntity} obj - Object that should be registered
+	 * @param {Function} downFunc - Function that is called when mousedown/touchstart is on obj
+	 * @param {Function} upFunc - Function that is called when mouseup/touchend is on obj
 	 */
 	registerClickable(obj) {
-		clickable.push(obj);
+		this.clickable.push(obj);
+	}
+
+	unregisterClickable(obj) {
+		for (var i = 0; i < this.clickable.length; i++) {
+			if(this.clickable[i] === obj) {
+				this.clickable.splice(i, 1);
+				return true;
+			}
+		}
+
+		console.warn("Could not unregister clickable");
+	}
+
+	notifyClickables(up) {
+		for (let i = 0; i < this.clickable.length; i++) {
+			let iObj = this.clickable[i];
+			if (collision.pointInRectangle(this.#x,
+			                               this.#y,
+			                               iObj.x - iObj.ox,
+			                               iObj.y - iObj.oy,
+			                               iObj.x - iObj.ox + iObj.width,
+			                               iObj.y - iObj.oy + iObj.height)) {
+				if (up)
+					iObj.clickUp();
+				else
+					iObj.clickDown();
+			}
+		
+		}
 	}
 
 	// Resets input state
 	reset() {
-		this.x = 0;
-		this.y = 0;
+		this.#x = 0;
+		this.#y = 0;
 
 		this.selected = undefined;
 		this.selectedTouch = undefined;
@@ -66,28 +98,29 @@ export default class Input {
 
 	updateCooordinates(event) {
 		let rect = this.g.painter.canvas.getBoundingClientRect();
-		this.setX(this.xScreenToInternal(event.clientX - rect.left));
-		this.setY(this.yScreenToInternal(event.clientY - rect.top));
+		this.#x = this.xScreenToInternal(event.clientX - rect.left);
+		this.#y = this.yScreenToInternal(event.clientY - rect.top);
 	}
 
-	// TODO remove?
-	setX(x) {
-		this.x = x;
+	getX() {
+		return this.#x;
 	}
 
-	// TODO remove?
-	setY(y) {
-		this.y = y;
+	getY() {
+		return this.#y;
 	}
 
 	touchstart(event) {
 		event.preventDefault(); // Prevent mouse events to fire as well
+
+		this.notifyClickables(false);
+
 		this.ensureOwner();
 		this.updateCooordinates(event.touches[0]);
 
 		// Planet selection
 		if (typeof this.selectedTouch === "undefined") {
-			let overBubble = this.g.collision_point(this.x, this.y, Base);
+			let overBubble = this.g.collision_point(this.#x, this.#y, Base);
 			if (typeof overBubble !== "undefined") {
 				// Start drag method
 				this.selectedTouch = overBubble;
@@ -102,16 +135,19 @@ export default class Input {
 
 	touchend(event) {
 		event.preventDefault(); // Prevent mouse events to fire as well
+
+		this.notifyClickables(true);
+
 		this.ensureOwner();
 		// TODO observer pattern
 		// Button clicking
-		let overButton = this.g.collision_point(this.x, this.y, Button)
+		let overButton = this.g.collision_point(this.#x, this.#y, Button)
 		if (typeof overButton !== "undefined") {
 			overButton.onClick()
 		}
 
 
-		let overBubble = this.g.collision_point(this.x, this.y, Base)
+		let overBubble = this.g.collision_point(this.#x, this.#y, Base)
 		if (typeof overBubble !== "undefined") {
 			if (typeof this.selectedTouch !== "undefined") {
 				if (overBubble === this.selectedTouch) {
@@ -129,11 +165,14 @@ export default class Input {
 
 	mousedown(event) {
 		this.updateCooordinates(event);
+
+		this.notifyClickables(false);
+
 		this.ensureOwner();
 
 		// Base selection
 		if (typeof this.selected === "undefined") {
-			let overBubble = this.g.collision_point(this.x, this.y, Base);
+			let overBubble = this.g.collision_point(this.#x, this.#y, Base);
 			if (typeof overBubble !== "undefined") {
 				// Start drag method
 				this.selected = overBubble;
@@ -148,15 +187,18 @@ export default class Input {
 
 	mouseup(event) {
 		this.updateCooordinates(event)
+
+		this.notifyClickables(true);
+
 		this.ensureOwner();
 
 		// Button clicking
-		let overButton = this.g.collision_point(this.x, this.y, Button)
+		let overButton = this.g.collision_point(this.#x, this.#y, Button)
 		if (typeof overButton !== "undefined") {
 			overButton.onClick()
 		}
 
-		let overBubble = this.g.collision_point(this.x, this.y, Base)
+		let overBubble = this.g.collision_point(this.#x, this.#y, Base)
 		if (typeof overBubble !== "undefined") {
 			if (typeof this.selected !== "undefined") {
 				if (overBubble === this.selected) {
@@ -182,9 +224,9 @@ export default class Input {
 
 			// Draw arrow from selected to cursor
 			let r = selectedBubble.width / 2;
-			if (!collision.pointInCircle(this.x, this.y, selectedBubble.x, selectedBubble.y, r)) {
-				let dx = this.x - selectedBubble.x;
-				let dy = this.y - selectedBubble.y;
+			if (!collision.pointInCircle(this.#x, this.#y, selectedBubble.x, selectedBubble.y, r)) {
+				let dx = this.#x - selectedBubble.x;
+				let dy = this.#y - selectedBubble.y;
 				let dist = Math.sqrt(dx**2 + dy**2);
 				let ndx = dx / dist;
 				let ndy = dy / dist;
@@ -192,7 +234,7 @@ export default class Input {
 				let starty = selectedBubble.y + ndy*r;
 				this.g.painter.ctx.strokeStyle = "white";
 				this.g.painter.ctx.lineWidth = 3;
-				this.g.painter.strokeLine(startx, starty, this.x, this.y);
+				this.g.painter.strokeLine(startx, starty, this.#x, this.#y);
 			}
 
 			// Highlight selected bubble
