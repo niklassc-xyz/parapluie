9c73ef353657b06ac377a3c3595b1a16fe63a580
niklassc <niklas@schmatloch.de>
2025-04-17 17:17:15 +0200

    remove base selection functionality → moved to game, improve clickable observer

--- Input.js
+++ Input.js
@@ -1,5 +1,3 @@
-import Button from "../parapluie/objects/util/Button.js";
-import Base from "../objects/bases/Base.js";
 import PhysicalEntity from "./objects/PhysicalEntity.js";
 import * as collision from "./functions/collision.js";
 
@@ -11,9 +9,6 @@ export default class Input {
 		this.g = g;
 		this.#x = 0;
 		this.#y = 0;
-
-		this.selected = undefined;
-		this.selectedTouch = undefined; // TODO merge again?
 
 		// For circle animation
 		this.circleCounterMax = 20;
@@ -37,64 +32,60 @@ export default class Input {
 	}
 
 	/**
-	 * Register obj as clickable
+	 * Register obj as clickable. ⚠ Entities need to unregister on destruction!
 	 *
-	 * @param {PhysicalEntity} obj - Object that should be registered
+	 * @param {DimensionEntity} obj - Object that should be registered
 	 * @param {Function} downFunc - Function that is called when mousedown/touchstart is on obj
 	 * @param {Function} upFunc - Function that is called when mouseup/touchend is on obj
 	 */
-	registerClickable(obj) {
-		this.clickable.push(obj);
+	registerClickable(ent, clickDownFn = ()=>{}, clickUpFn = ()=>{}) {
+		this.clickable.push({
+			entity: ent,
+			clickDown: clickDownFn,
+			clickUp: clickUpFn,
+		});
 	}
 
-	unregisterClickable(obj) {
+	unregisterClickable(entity) {
 		for (var i = 0; i < this.clickable.length; i++) {
-			if(this.clickable[i] === obj) {
+			if(this.clickable[i].entity === entity) {
 				this.clickable.splice(i, 1);
 				return true;
 			}
 		}
 
-		console.warn("Could not unregister clickable");
+		console.warn("Tried unregistering non-registered clickable");
+		return false;
 	}
 
 	notifyClickables(up) {
 		for (let i = 0; i < this.clickable.length; i++) {
-			let iObj = this.clickable[i];
+			let iEnt = this.clickable[i].entity;
 			if (collision.pointInRectangle(this.#x,
 			                               this.#y,
-			                               iObj.x - iObj.ox,
-			                               iObj.y - iObj.oy,
-			                               iObj.x - iObj.ox + iObj.width,
-			                               iObj.y - iObj.oy + iObj.height)) {
+			                               iEnt.x - iEnt.ox,
+			                               iEnt.y - iEnt.oy,
+			                               iEnt.x - iEnt.ox + iEnt.width,
+			                               iEnt.y - iEnt.oy + iEnt.height)) {
 				if (up)
-					iObj.clickUp();
+					// console.log("BUTTON UP");
+					this.clickable[i].clickUp();
+					// iEnt.clickUp();
 				else
-					iObj.clickDown();
+					// console.log("BUTTON DOWN");
+					this.clickable[i].clickDown();
+					// iEnt.clickDown();
 			}
 		
 		}
 	}
 
 	// Resets input state
-	reset() {
-		this.#x = 0;
-		this.#y = 0;
+	// reset() {
+	// 	this.#x = 0;
+	// 	this.#y = 0;
+	// }
 
-		this.selected = undefined;
-		this.selectedTouch = undefined;
-	}
-
-	// Checks if selected bubbles are still owned, otherwise clears
-	ensureOwner() {
-		if (this.selected !== undefined && this.selected.team !== 1) {
-			this.selected = undefined;
-		}
-
-		if (this.selectedTouch !== undefined && this.selectedTouch.team !== 1) {
-			this.selectedTouch = undefined;
-		}
-	}
 
 	updateCooordinates(event) {
 		let rect = this.g.painter.canvas.getBoundingClientRect();
@@ -114,18 +105,7 @@ export default class Input {
 		event.preventDefault(); // Prevent mouse events to fire as well
 
 		this.notifyClickables(false);
-
-		this.ensureOwner();
 		this.updateCooordinates(event.touches[0]);
-
-		// Planet selection
-		if (typeof this.selectedTouch === "undefined") {
-			let overBubble = this.g.collision_point(this.#x, this.#y, Base);
-			if (typeof overBubble !== "undefined") {
-				// Start drag method
-				this.selectedTouch = overBubble;
-			}
-		}
 	}
 
 	touchmove(event) {
@@ -137,127 +117,22 @@ export default class Input {
 		event.preventDefault(); // Prevent mouse events to fire as well
 
 		this.notifyClickables(true);
-
-		this.ensureOwner();
-		// TODO observer pattern
-		// Button clicking
-		let overButton = this.g.collision_point(this.#x, this.#y, Button)
-		if (typeof overButton !== "undefined") {
-			overButton.onClick()
-		}
-
-
-		let overBubble = this.g.collision_point(this.#x, this.#y, Base)
-		if (typeof overBubble !== "undefined") {
-			if (typeof this.selectedTouch !== "undefined") {
-				if (overBubble === this.selectedTouch) {
-					this.selectedTouch = overBubble;
-				} else {
-					this.selectedTouch.attack(overBubble);
-					this.selectedTouch = undefined;
-				}
-
-			}
-		} else {
-			this.selectedTouch = undefined;
-		}
 	}
 
 	mousedown(event) {
 		this.updateCooordinates(event);
-
 		this.notifyClickables(false);
-
-		this.ensureOwner();
-
-		// Base selection
-		if (typeof this.selected === "undefined") {
-			let overBubble = this.g.collision_point(this.#x, this.#y, Base);
-			if (typeof overBubble !== "undefined") {
-				// Start drag method
-				this.selected = overBubble;
-			}
-		}
 	}
 
 	mousemove(event) {
 		this.updateCooordinates(event)
-		// this.ensureOwner(); // TODO check if necessary on move → is executed a lot
 	}
 
 	mouseup(event) {
 		this.updateCooordinates(event)
 
 		this.notifyClickables(true);
-
-		this.ensureOwner();
-
-		// Button clicking
-		let overButton = this.g.collision_point(this.#x, this.#y, Button)
-		if (typeof overButton !== "undefined") {
-			overButton.onClick()
-		}
-
-		let overBubble = this.g.collision_point(this.#x, this.#y, Base)
-		if (typeof overBubble !== "undefined") {
-			if (typeof this.selected !== "undefined") {
-				if (overBubble === this.selected) {
-					this.selected = overBubble;
-				} else {
-					this.selected.attack(overBubble);
-					this.selected = undefined;
-				}
-
-			}
-		} else {
-			this.selected = undefined;
-		}
 	}
-
-	// Draw circle indicator arround selectedBubble and arrow
-	selectedDrawing(selectedBubble) {
-		if(selectedBubble !== undefined) {
-			// Cancel if selected bubble has been captured in the meantime
-			if(selectedBubble.team !== 1) {
-				selectedBubble = undefined; return;
-			}
-
-			// Draw arrow from selected to cursor
-			let r = selectedBubble.width / 2;
-			if (!collision.pointInCircle(this.#x, this.#y, selectedBubble.x, selectedBubble.y, r)) {
-				let dx = this.#x - selectedBubble.x;
-				let dy = this.#y - selectedBubble.y;
-				let dist = Math.sqrt(dx**2 + dy**2);
-				let ndx = dx / dist;
-				let ndy = dy / dist;
-				let startx = selectedBubble.x + ndx*r;
-				let starty = selectedBubble.y + ndy*r;
-				this.g.painter.ctx.strokeStyle = "white";
-				this.g.painter.ctx.lineWidth = 3;
-				this.g.painter.strokeLine(startx, starty, this.#x, this.#y);
-			}
-
-			// Highlight selected bubble
-			this.g.painter.ctx.lineWidth = 2;
-			this.g.painter.ctx.strokeStyle = "white";
-			for(let i = 0; i < 5 + Math.abs(this.circleCounter - this.circleCounterMax/2); i+=3) {
-				this.g.painter.strokeCircle(
-							selectedBubble.x,
-							selectedBubble.y,
-							selectedBubble.width / 2 + i,
-							);
-			}
-
-			this.circleCounter = (this.circleCounter + 1) % this.circleCounterMax;
-		}
-
-	}
-
-	draw() {
-		this.selectedDrawing(this.selected);
-		this.selectedDrawing(this.selectedTouch);
-	}
-
 
 	// Converts xD to x
 	xScreenToInternal(xD) {
