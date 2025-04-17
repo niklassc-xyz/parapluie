1367d7d52046b273c7eede9482801700b7b620ab
niklassc <niklas@schmatloch.de>
2025-04-17 21:54:08 +0200

    support both path and image as sprite

--- objects/SpriteEntity.js
+++ objects/SpriteEntity.js
@@ -2,17 +2,32 @@ import PhysicalEntity from "./PhysicalEntity.js";
 import * as math from "../functions/math.js";
 
 export default class SpriteEntity extends PhysicalEntity {
+	/**
+	 * A PhysicalEntity that has a sprite
+	 *
+	 * @param {Game reference} g - Game reference
+	 * @param {x coordinate} x - x-coordinate
+	 * @param {y coordinate} y - y-coordinate
+	 * @param {Width of the entity} width - Width of the entity
+	 * @param {Height of the entity} height - Height of the entity
+	 * @param {string|OffscreenCanvas} sprite - Sprite of the entity, can be either image or path
+	 */
 	constructor(g, x, y, width, height, sprite) {
 		super(g, x, y, width, height);
 
-		this.sprite = sprite;
+		if (typeof sprite === "string") {
+			this.sprite = this.g.resourceManager.getSpriteFromPath(sprite);
+		} else {
+			// TODO check that sprite is an image
+			this.sprite = sprite;
+		}
 	}
 
 	draw() {
 		super.draw();
 		
 		// Draw sprite
-		if(this.direction !== 0) {
+		if (this.direction !== 0) {
 			// Rotate before drawing
 			this.g.painter.ctx.save();
 			this.g.painter.ctx.translate(this.x, this.y);
