6b37f39c146a929f2e3d99541a48f709a0aeedc7
niklassc <niklas@schmatloch.de>
2025-04-22 17:19:47 +0200

    add distance()

--- functions/math.js
+++ functions/math.js
@@ -28,3 +28,8 @@ export function pointDirection(x1, y1, x2, y2) { // TODO test
 export function mMod(a, b) {
 	return ((a % b + b) % b);
 }
+
+// Returns distance between (x1|y2) and (x2|y2)
+export function distance(x1, y1, x2, y2) {
+	return Math.sqrt((x2-x1)^2 + (y2-y1)^2);
+}
